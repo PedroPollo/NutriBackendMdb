5c074a87ebf166798f9cfec761bd49fde2f8ac23
PedroPollo <72528531+PedroPollo@users.noreply.github.com>
2025-08-26 19:36:45 -0600

    Correcciones

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), '0.0.0.0', () => {
     console.log("Server lintening on port ", app.get('port'));
 })
--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 module.exports = {
     app: {
-        port: process.env.PORT,
+        port: process.env.PORT || 3002,
     },
     jwt: {
         secret: process.env.JET_SECRET || 'notaSecreta!'
     },
     mongo: {
-        db: process.env.MONGODB,
+        db: process.env.MONGODB || 'mongodb+srv://read-write:nutripob@cluster0.aacbi.mongodb.net/nutri?retryWrites=true&w=majorityc',
     }
 }
